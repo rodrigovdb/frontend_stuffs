4862911176209956bc2f8514d12944024a22e921
Rodrigo Otavio van den Berg Maia <rodrigo@ladodireito.com>
2015-07-28 13:34:47 +0000

    Pipe compiling fixes

--- gulpfile.js
+++ gulpfile.js
@@ -5,31 +5,59 @@ var gulp        = require('gulp'),
     gutil       = require('gulp-util'),
     coffee      = require('gulp-coffee'),
     jade        = require('gulp-jade'),
-    browserSync = require('browser-sync');
+    browserSync = require('browser-sync'),
+    ghPages     = require('gulp-gh-pages'),
+    concat      = require('gulp-concat'),
+    order       = require('gulp-order'),
+    gulpif      = require('gulp-if');
 
 var paths = {
-  jquery            : './node_modules/jquery/dist/jquery.min.js',
-  fontawesome_css   :  './node_modules/font-awesome/scss/font-awesome.scss',
-  fontawesome_font  : './node_modules/font-awesome/fonts/*',
+  sass      : './src/sass/*.scss',
+  coffee    : './src/coffee/*.coffee',
+  jade      : './src/jade/*.jade',
 
-  sass              : './src/sass/*.scss',
-  coffee            : './src/coffee/*.coffee',
-  jade              : './src/jade/*.jade'
+  images    : './src/images/*',
+
+  jquery        : './bower_components/jquery/dist/jquery.min.js',
+  angular       : './bower_components/angular/angular.min.js',
+  bootstrap_js  : './bower_components/bootstrap/dist/js/bootstrap.min.js',
+  bootstrap_css : './bower_components/bootstrap/dist/css/*.min.css'
 }
 
-gulp.task('sass', function() {
-    gulp.src(paths.sass)
-        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+gulp.task('sass', function(){
+    gulp.src([
+          paths.bootstrap_css,
+          paths.sass
+        ])
+        .pipe(gulpif(/\.scss$/, sass({ style: 'compressed' })))
+        .pipe(order([
+          'bootstrap.min.css',
+          'bootstrap-theme.min.css',
+          'src/sass/*.scss'
+        ]))
+        .pipe(concat('./main.css'))
         .pipe(gulp.dest('./build/css/'));
 });
 
-gulp.task('coffee', function() {
-  gulp.src(paths.coffee)
-      .pipe(coffee({bare: true}).on('error', gutil.log))
+gulp.task('coffee', function(){
+  gulp.src([
+        paths.jquery,
+        paths.angular,
+        paths.bootstrap_js,
+        paths.coffee,
+      ])
+      .pipe(gulpif(/\.coffee$/, coffee({bare: true})))
+      .pipe(order([
+        'jquery.min.js',
+        'angular.min.js',
+        'bootstrap.min.js',
+        'src/coffee/*.coffee'
+      ]))
+      .pipe(concat('./main.js'))
       .pipe(gulp.dest('./build/js/'))
 });
 
-gulp.task('jade', function() {
+gulp.task('jade', function(){
   var YOUR_LOCALS = {};
 
   gulp.src(paths.jade)
@@ -37,6 +65,12 @@ gulp.task('jade', function() {
       .pipe(gulp.dest('./build/'))
 });
 
+gulp.task('images', function () {
+  gulp.src(paths.images)
+      .pipe(gulp.dest('./build/img'));
+});
+
+gulp.task('copy', ['images']);
 gulp.task('compile', ['sass', 'coffee', 'jade']);
 
 gulp.task('server', function () {
@@ -47,9 +81,14 @@ gulp.task('server', function () {
 });
 
 gulp.task('watch', function(){
-  gulp.watch(paths.sass,    ['sass']);
-  gulp.watch(paths.coffee,  ['coffee']);
-  gulp.watch(paths.jade,    ['jade']);
+  gulp.watch(paths.sass,                ['sass']);
+  gulp.watch(paths.coffee,              ['coffee']);
+  gulp.watch('./src/jade/**/*.jade',    ['jade']);
 });
 
-gulp.task('default', ['compile', 'server', 'watch']);
+gulp.task('deploy', ['compile'], function() {
+  return gulp.src('./build/**/*')
+             .pipe(ghPages());
+});
+
+gulp.task('default', ['copy', 'compile', 'server', 'watch']);
